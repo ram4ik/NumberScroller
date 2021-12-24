57b0361b3b37606c467b3a2d25992996616a2aca
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-24 13:20:57 +0200

    Initial Commit

---
+++ NumberScroller WatchKit Extension/NumberScrollerApp.swift
@@ -0,0 +1,19 @@
+//
+//  NumberScrollerApp.swift
+//  NumberScroller WatchKit Extension
+//
+//  Created by ramil on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct NumberScrollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
